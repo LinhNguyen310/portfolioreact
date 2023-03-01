ffdf60d51373dc57934a65178bc93e4f90543d9b
linh <lhnguye1@ualberta.ca>
2023-03-01 09:06:46 -0700

    add resume

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,9 +7,64 @@ import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
-import resume from '../assets/resume/resume.pdf';
 
 export const NavBar = () => {
+    const storageKey = 'theme-preference'
+    const onClick = () => {
+    // flip current value
+    theme.value = theme.value === 'light'
+        ? 'dark'
+        : 'light'
+    setPreference()    
+    }
+
+    const getColorPreference = () => {
+    if (localStorage.getItem(storageKey))
+        return localStorage.getItem(storageKey)
+    else
+        return window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light'
+    }
+
+    const setPreference = () => {
+    localStorage.setItem(storageKey, theme.value)
+    reflectPreference()
+    }
+
+    const reflectPreference = () => {
+    document.firstElementChild
+        .setAttribute('data-theme', theme.value)
+
+    document
+        .querySelector('#theme-toggle')
+        ?.setAttribute('aria-label', theme.value)
+    }
+
+    const theme = {
+    value: getColorPreference(),
+    }
+
+    // set early so no page flashes / CSS is made aware
+    reflectPreference()
+
+    window.onload = () => {
+    // set on load so screen readers can see latest value on the button
+    reflectPreference()
+
+    // now this script can find and listen for clicks on the control
+    document
+        .querySelector('#theme-toggle')
+        .addEventListener('click', onClick)
+    }
+
+    // sync with system changes
+    window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({matches:isDark}) => {
+        theme.value = isDark ? 'dark' : 'light'
+        setPreference()
+    })
     const [activeLink, setActiveLink] = useState('home')
     const [scrolled, seScrolled] = useState(false)
     useEffect(() =>
@@ -49,20 +104,43 @@ export const NavBar = () => {
                 {/* // highlight the link at the top
                 // check if the active link == home. If not, the name will be navbar-link
                 // onClick = check if the title is clicked   */}
+            
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
+            
             {/* // add social contact */}
             <span className='navbar-text'>
+            <button class="theme-toggle" id="theme-toggle" title="Toggles light & dark" aria-label="auto" aria-live="polite">
+                <svg class="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
+                    <mask class="moon" id="moon-mask">
+                    <rect x="0" y="0" width="100%" height="100%" fill="white" />
+                    <circle cx="24" cy="10" r="6" fill="black" />
+                    </mask>
+                    <circle class="sun" cx="12" cy="12" r="6" mask="url(#moon-mask)" fill="currentColor" />
+                    <g class="sun-beams" stroke="currentColor">
+                    <line x1="12" y1="1" x2="12" y2="3" />
+                    <line x1="12" y1="21" x2="12" y2="23" />
+                    <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+                    <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+                    <line x1="1" y1="12" x2="3" y2="12" />
+                    <line x1="21" y1="12" x2="23" y2="12" />
+                    <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+                    <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+                    </g>
+                </svg>
+            </button>
                 <div className='social-icon'>
                     <a href = "add social contact here"><img src = {navIcon1} alt=""></img></a>
                     <a href = "add social contact here"><img src = {navIcon2} alt=""></img></a>
                     <a href = "add social contact here"><img src = {navIcon3} alt=""></img></a>
                 </div>
-                <a href="../assets/resume/resume.pdf" download="resume">
+                <a download href="../assets/img/nav-icon3.svg">
                     <button type="button">Download Resume</button>
-                    </a>            </span>
+                    </a>            
+                </span>
+                
         </Navbar.Collapse>
         </Container>
     </Navbar>
