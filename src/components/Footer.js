f8b9dffc6881566eb87e615f502433b135a3d2c6
linh <lhnguye1@ualberta.ca>
2023-03-03 00:04:56 -0700

    fied resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ export const Footer = () => {
               <a href = "https://www.instagram.com/linhnguy_en/"><img src = {navIcon3} alt=""></img></a>
           </div>
           <span className='footer-text'>
-          <a download="linh'sresume.pdf" href="./assets/img/linh'sresume.pdf">
+          <a href="https://drive.google.com/file/d/1kbH4ES18K5ALOAOW9wl0AkiF762G2UhO/view?usp=sharing">
               <button type="button">
                 Download Resume
                 </button>
